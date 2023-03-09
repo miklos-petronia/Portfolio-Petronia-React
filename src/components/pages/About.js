b0c9a33d04130b1a48eb9aa513a507088ab955ab
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-09 18:08:20 +0100

    added the about js file

---
+++ src/components/pages/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import "../styles/About.css"
+import Header from "../Header"
+import { Container } from "react-bootstrap"
+import backgroundImage from "../assets/images/profilepicture.png"
+
+
+const backgroundStyles = {
+    backgroundImage: `url(${backgroundImage})`,
+    height: '400px'
+};
+export default function About() {
+    return (
+        <div>
+            <Header></Header>
+            <div>
+                <div className='container'>
+                    <div className='text-white'>
+                        <p className='mb-3 mt-3'> My name is Miklos Petronia, and I am from the island of Aruba. I am a business developer with a master of science degree in strategic entrepreneurship from Erasmus University, located; in the Netherlands. I love to challenge myself and exceed expectations. My passion is entrepreneurship, bringing innovation into my work,  and achieving the best possible result. As a result, I took it upon myself to learn to code and become a full-stack web developer to complement my learned entrepreneurial skillsets. I am excited to see what I will create and collaborate with other like-minded individuals to develop something that would benefit people's daily lives. It is my first step to learning programming,  and there is more to come!</p>
+                    </div>
+                </div>
+            </div>
+            <div
+                className='p-5 img-fluid '
+                style={backgroundStyles}
+            >
+            </div>
+
+        </div>
+    );
+}
