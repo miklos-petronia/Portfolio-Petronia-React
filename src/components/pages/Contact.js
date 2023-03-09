07473f9095db4639824e1b8f5af0a90f6f6fb488
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-09 18:09:28 +0100

    added the contact js

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from "../Form"
+import { Container } from "react-bootstrap"
+export default function Contact() {
+    return (
+        <div>
+            <Container style={{ height: '88vh' }}>
+                <Form />
+            </Container>
+        </div>
+    );
+}
