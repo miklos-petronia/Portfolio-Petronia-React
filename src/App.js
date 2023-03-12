39a882bbb3400a086d6c1076c3c7463655aa1ff7
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-12 02:57:52 +0100

    portfilio container

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+
+function App() {
+
+    return (
+        <div className='bg-dark'>
+            <PortfolioContainer />
+        </div>
+    );
+}
+
+
+export default App;
