896f5e82e2e46c3db93d4cbf3935f9c9ae19632a
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-12 02:51:27 +0100

    presenting the value present page and returning corresponding component to render

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume'
+import Footer from './Footer'
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    // What vallue` present Page` is. Return the corresponding component to render.
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <About />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        return <Resume />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div >
+            {/* Passing the currentPage from state and the function to update it */}
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+            {/* Calling the renderPage method which will return a component  */}
+            {renderPage()}
+            <Footer />
+        </div>
+    );
+}
